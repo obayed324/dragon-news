50bef41fdc5df5345cd10205c303aa5a88847f66
Mohammad Obayed Sarker <127292015+obayed324@users.noreply.github.com>
2025-10-18 21:39:35 +0600

    sticky scroll behaviour added

--- src/Layouts/HomeLayOute.jsx
+++ src/Layouts/HomeLayOute.jsx
@@ -21,7 +21,7 @@ const HomeLayout = () =>{
 
             <main className="w-11/12 mx-auto gap-5 grid grid-cols-12">
                 
-                <aside className="col-span-3">
+                <aside className="col-span-3 sticky top-0 h-fit">
                     <LeftAside></LeftAside>
                 </aside>
 
@@ -29,7 +29,7 @@ const HomeLayout = () =>{
                     <Outlet></Outlet>
                 </section>
 
-                <aside className="col-span-3">
+                <aside className="col-span-3 sticky top-0 h-fit">
                     <RightAside></RightAside>
                 </aside>
 
--- src/components/HomeLayout/QZone/QZone.jsx
+++ src/components/HomeLayout/QZone/QZone.jsx
@@ -6,7 +6,7 @@ const QZone = () => {
     return (
         <div className='bg-base-200 p-3'>
             <h2 className='font-bold nb-5'>QZone</h2>
-            <div className='space-y-5'>
+            <div className='space-y-5b '>
                 <img src={swimmingImage} alt="" />
                 <img src={classImage} alt="" />
                 <img src={playGround} alt="" />
